485c6115ce9c544af5bb4b0400856e9bbf969dee
Jade Weathington <jadeweathington@gmail.com>
2024-06-05 21:16:13 -0400

    Removed placeholder stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,24 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [messageFromServer, setMessageFromServer] = useState("");
+  // const [messageFromServer, setMessageFromServer] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("/api/message");
-      const data = await response.json();
-      setMessageFromServer(data.message);
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch("/api/message");
+  //     const data = await response.json();
+  //     setMessageFromServer(data.message);
+  //   };
 
-    fetchData().catch((e) => console.error(e));
-  }, []);
+  //   fetchData().catch((e) => console.error(e));
+  // }, []);
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Golang + Vite + React!!!!!!!</h1>
-      <h2>{messageFromServer}</h2>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>Placeholder</h1>
     </>
   );
 }
